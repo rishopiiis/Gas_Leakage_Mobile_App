87fdab735fa3c6e1722f8e4030905093aaf4be6a
rishopiiis <e20371@eng.pdn.ac.lk>
2025-09-09 09:22:24 +0530

    safety and gasdata

---
+++ FrontEnd_MobileApp/app/Components/GasData/GasData.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class GasData extends Component {
+  render() {
+    return (
+      <View>
+        <Text>GasData profile</Text>
+      </View>
+    )
+  }
+}
+
+export default GasData
